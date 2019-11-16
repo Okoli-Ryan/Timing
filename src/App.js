e07d37989b3df584547209a45e2d0af257354a61
unknown <okoliryan50@gmail.com>
2019-11-16 15:15:07 +0100

    editted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from '../node_modules/react-router-dom';
 import About from './About';
 import Timer from './Timer';
 import Navigation from "./Nav";
